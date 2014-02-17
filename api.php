33d820a97f86deab75929ac2ce246dfcdaab4fd8
James Hemery <contact@james-hemery.fr>
2014-02-17 20:25:44 +0100

    Ajout d'informations et refonte du code
    • Integration de la class simple_html_dom.php
    • Ajout de la récupération des thumbs
    • Ajout de la récupération des dates
    • Ajout de la récupération thumb

--- api.php
+++ api.php
@@ -1,43 +1,41 @@
+<meta charset="utf-8">
 <?php
-if(!preg_match('#^([a-zA-Z0-9]+)$#',$_GET["v"])) { die("Error : Invalid Video ID (#0)");} // L'id ne correspond pas aux formats requis.
-$videoID = $_GET["v"];
+    require_once('simple_html_dom.php');
+    if(!preg_match('#^([a-zA-Z0-9]+)$#',$_GET["v"])) { die("Error : Invalid Video ID (#0)");} // L'id ne correspond pas aux formats requis.
+    $videoID = $_GET["v"];
 
-$url = "http://dreamvids.fr/index.php?page=watch&vid=".$videoID;
+    $url = "http://dreamvids.fr/index.php?page=watch&vid=".$videoID;
 
-/* On récupère la page de la vidéo */
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_USERAGENT, 'API for DreamVids');
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-$headers = array ('Accept-Language:fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4');
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);;
-$video = curl_exec($ch);
-curl_close($ch);
+    $html = new simple_html_dom();
+    $html->load_file($url);
+    $results = array();
 
-if(utf8_decode($video) == "Une erreur est survenue lors du chargement de la vidéo") { die("Error : Invalid Video ID (#1)");} // DreamVids a répondu un message d'erreur
+    // On récupère le titre
+    $results['title'] = $html->find('h1', 0)->plaintext;
 
-/* On parse */
-// On enlève les retours à la ligne
-$video = str_replace("\n", "", $video);
-$video = str_replace("\r", "", $video);
+    // On récupère l'auteur
+    $author = $html->find('h1>small a', 0);
+    $results['author'] = $author->plaintext;
+    $results['author_link'] = 'http://dreamvids.fr'.$author->href;
 
-// On récupère le titre et l'auteur de la vidéo
-preg_match('#<h1>(.+)<small> par <a href="/@(.+)">.+</a></small></h1>#',$video, $match);
-$resultat["title"] = $match[1];
-$resultat["author"] = $match[2];
+    // On récupère la description
+    $results['description'] = $html->find('div.panel-body', 0)->plaintext;
+    // $results['description'] = str_replace("\n", '', $results['description']);
+    // $results['description'] = str_replace("\r", '', $results['description']);
+    $results["description"] = str_replace("\t\t\t", "", $results['description']);
+    $results["description"] = htmlspecialchars($results['description']);
 
-// On récupère la description de la vidéo
-preg_match('#<div class="panel-body">(.+)</div>	</div>#',$video, $match);
-$resultat["description"] = str_replace("\t\t\t", "", $match[1]);
-$resultat["description"] = htmlspecialchars($resultat["description"]);
+    // On récupère les vues
+    $results['views'] = $html->find('b[style]', 0)->plaintext;
 
-// On récupère le nombre de vues
-preg_match('#<b style="margin-left:50px">([0-9]+) vues</b>#',$video, $match);
-$resultat["vues"] = $match[1];
+    // On récupère la date
+    $date = preg_match('#le ([0-9\/]+) à ([0-9\:]+)#',  $html->find('div.panel-heading', 0)->plaintext, $match);
+    $results['date'] = $match[1];
+    $results['hour'] = $match[2];
 
-/* On encode et on affiche */
-$json = json_encode($resultat);
-print_r($json);
+    // On récupère l'image à la une
+    $results['thumb'] = 'http://dreamvids.fr/'.$html->find('video', 0)->poster;
+
+    $json = json_encode($results);
+    var_dump($json);
 ?>
