de344e3c1db40abf4560a5ad220851901e11ff64
James Hemery <contact@james-hemery.fr>
2014-02-18 18:02:32 +0100

    Petite modification pour l'autonomie.

--- apiDreamVids.php
+++ apiDreamVids.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('simple_html_dom.php');
+    require_once(__DIR__.DS.'simple_html_dom.php');
     $html = new simple_html_dom();
     class apiDreamVids{
         public function getVideoInformations($id_video){
